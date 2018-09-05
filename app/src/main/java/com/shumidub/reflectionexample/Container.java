fa0cb8ea8cfcb791e80fef8ef54c58d713421c3a
ShumidubCoder <shumidubcoder@gmail.com>
2018-09-05 04:09:21 +0300

    add constructors and methods to Container class

--- app/src/main/java/com/shumidub/reflectionexample/Container.java
+++ app/src/main/java/com/shumidub/reflectionexample/Container.java
@@ -4,7 +4,21 @@ public class Container {
     private String privateString = "privateString";
     final boolean booleanValue;
 
-    public Container(boolean booleanValue) {
+    Container() { this.booleanValue = false; }
+
+    Container(boolean booleanValue) {
         this.booleanValue = booleanValue;
     }
+
+    Container(boolean booleanValue, int t) {
+        this.booleanValue = booleanValue;
+    }
+
+    private void firstPrivateMethod(){}
+
+    private String secondPrivateMethod(){ return "Result from second private method";}
+
+    private String thirdPrivateMethod(String argument){ return "The argument is - " + argument;}
+
+
 }
