25ac495591dc29f128dea6253debf0eb886a255c
ShumidubCoder <shumidubcoder@gmail.com>
2018-09-07 06:12:59 +0300

    add some unit tests

--- app/src/test/java/com/shumidub/reflectionexample/ExampleUnitTest.java
+++ app/src/test/java/com/shumidub/reflectionexample/ExampleUnitTest.java
@@ -2,6 +2,13 @@ package com.shumidub.reflectionexample;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
+import static com.shumidub.reflectionexample.ReflectionHelper.getConstructors;
+import static com.shumidub.reflectionexample.ReflectionHelper.getPrivateField;
+import static com.shumidub.reflectionexample.ReflectionHelper.getPrivateMethodReturnType;
+import static com.shumidub.reflectionexample.ReflectionHelper.invokePrivateMethod;
+import static com.shumidub.reflectionexample.ReflectionHelper.setNewValueForImmutableField;
 import static org.junit.Assert.*;
 
 /**
@@ -10,8 +17,50 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+    private Container container = new Container();
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void getPrivateFieldMethod_workCorrect() {
+        assertEquals("privateString",
+                getPrivateField(container, "privateString"));
+    }
+
+    @Test
+    public void setNewValueForImmutableField_workCorrect() {
+        boolean oldValue = container.booleanValue;
+        boolean newValue = !container.booleanValue;
+        setNewValueForImmutableField(container, "booleanValue", newValue);
+        assertNotEquals(oldValue, container.booleanValue);
+        assertEquals(newValue, container.booleanValue);
+    }
+
+    @Test
+    public void invokePrivateMethod_workCorrect() {
+        //void method return null
+        assertNull(invokePrivateMethod(container, "firstPrivateMethod", null));
+
+        //"Result from second private method" expected as result from container.secondPrivateMethod();
+        assertEquals("Result from second private method",
+                invokePrivateMethod(container, "secondPrivateMethod", null));
+    }
+
+
+    @Test
+    public void getPrivateMethodReturnType_workCorrect() {
+        //void method return null
+        assertEquals("void", getPrivateMethodReturnType(container,
+                "firstPrivateMethod").toString());
+
+        //String.class is expected return type from container.secondPrivateMethod();
+        assertEquals(String.class,
+                getPrivateMethodReturnType(container, "secondPrivateMethod"));
+    }
+
+
+    @Test
+    public void getConstructors_workCorrect() {
+        System.out.println(Arrays.toString(getConstructors(Container.class).toArray()));
+        assertEquals(3, getConstructors(Container.class).size());
     }
 }
